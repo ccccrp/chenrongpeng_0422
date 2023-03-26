5d0e69ed409c5c2a21e37ec3ab4b4de887042fd5
chenrongpeng <2197872080@qq.com>
2023-03-26 15:00:11 +0800

    第一次提交

---
+++ crp/crpDlg.h
@@ -0,0 +1,34 @@
+﻿
+// crpDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CcrpDlg 对话框
+class CcrpDlg : public CDialogEx
+{
+// 构造
+public:
+	CcrpDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CRP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
